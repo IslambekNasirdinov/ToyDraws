2ce4c1f8b1c479a7ac8676cb79233615bc9725c3
IslambekNasirdinov <uolter91@mail.ru>
2024-03-20 21:57:42 +0300

    Контрольная задача №2

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    private static final String FILENAME = "toys.csv";
+    public static void main(String[] args) {
+        try {
+            Market market = new Market();
+            File toyFile = new File(FILENAME);
+
+            if(!toyFile.exists()){
+                toyFile.createNewFile();
+                market.addToys(new Toys(1,"Машина", 70,30));
+                market.addToys(new Toys(2,"Мишка",100,35));
+                market.addToys(new Toys(3,"Вини",50,40));
+                market.saveToFile(FILENAME);
+            }else {
+                market.loadFile(FILENAME);
+            }
+            market.setWeight(2,80); // Изменение количество частота выпадения игрушки (вес в % от 100)
+            ArrayList<String> toyList = market.getToyList();
+            for(String toy : toyList){
+                System.out.println(toy);
+            }
+            ArrayList<Toys> winners = market.playGame(1);
+            System.out.println("Выиграли следующие игрушки: ");
+            for(Toys t : winners){
+                System.out.println(t.getName());
+            }
+            market.saveToFile(FILENAME);
+        }catch (IOException e){
+            System.out.println("Ошибка при работе с файлами: " +e.getMessage());
+        }
+
+
+    }
+}
---
+++ src/Market.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Market {
+    private ArrayList<Toys> toys;
+
+    public Market(){
+        toys = new ArrayList<>();
+    }
+
+    public void addToys(Toys toy){
+        boolean foundToy = false;
+        for (Toys t : toys){
+            if(t.getId() == toy.getId()){
+                t.setQuantity(t.getQuantity() + toy.getQuantity());
+                foundToy = true;
+                break;
+            }
+        }
+        if(!foundToy){
+            toys.add(toy);
+        }
+    }
+    public void setWeight(int toyId, double weight){
+        for(Toys t :toys){
+            if (t.getId() == toyId){
+                t.setWeight(weight);
+                break;
+            }
+        }
+    }
+    public ArrayList<String> getToyList(){
+        ArrayList<String> toyList = new ArrayList<>();
+        for (Toys t :toys){
+            toyList.add("ID: " + t.getId() + "Название: " + t.getName() +
+                    "Количество: " + t.getQuantity() + "Вес: " + t.getWeight());
+        }
+        return toyList;
+    }
+    public ArrayList<Toys> playGame(int count){
+        ArrayList<Toys> winners = new ArrayList<>();
+        double weightSum = 0;
+        for(Toys t : toys){
+            weightSum += t.getWeight();
+        }
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            double randomNumber = random.nextDouble() * weightSum;
+            double currentSum = 0;
+            for(Toys t : toys){
+                currentSum += t.getWeight();
+                if(currentSum >= randomNumber){
+                    if(t.getQuantity() > 0){
+                        winners.add(t);
+                        t.setQuantity(t.getQuantity() -1);
+                        weightSum -= t.getWeight();
+                    }
+                    break;
+                }
+            }
+            
+        }
+        return winners;
+    }
+    public void saveToFile(String fileName) throws IOException {
+        try(FileWriter writer = new FileWriter(new File(fileName))) {
+            for(Toys t :toys){
+                writer.write(t.getId() + "," + t.getName() + "," +
+                        t.getQuantity() + "," + t.getWeight() + "\n");
+            }
+
+        }
+
+    }
+    public void loadFile(String fileName) throws IOException{
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                String[] toyDate = line.split(",");
+                Toys toy = new Toys(Integer.parseInt(toyDate[0]),toyDate[1],Integer.parseInt(toyDate[2]),Double.parseDouble(toyDate[3]));
+                toys.add(toy);
+            }
+
+        }
+    }
+
+
+}
